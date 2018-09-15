f52b76920cc141f88175c5d980c5ce1229d8e00b
Akash Gupta <gupta.akash88@gmail.com>
2018-09-16 00:20:14 +0530

    Revert "Updates"
    This reverts commit d25fbbd9df874b577b80ea739b21e79c8cfafc81.

--- path----2e8291ca9c1b8fec6794.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{104:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Akash Gupta"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----2e8291ca9c1b8fec6794.js.map
